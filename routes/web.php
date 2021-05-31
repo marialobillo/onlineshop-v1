7049749aaad259b7ea28b02253ca942d86dd1d6e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-05-31 13:42:50 +0200

    adding main controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 /*
@@ -13,24 +14,22 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('main');
+Route::get('/', [MainController::class, 'index'])->name('main');
 
-Route::get('products', 
+Route::get('products',
     [ProductController::class, 'index'])->name('products.index');
 
-Route::get('products/create', 
+Route::get('products/create',
     [ProductController::class, 'create'])->name('products.create');
 
-Route::post('products', 
+Route::post('products',
     [ProductController::class, 'store'])->name('products.store');
 
-Route::get('products/{product}', 
+Route::get('products/{product}',
     [ProductController::class, 'show'])->name('products.show');
 
-Route::match(['put', 'patch'], 'products/{product}', 
+Route::match(['put', 'patch'], 'products/{product}',
     [ProductController::class, 'edit'])->name('products.update');
 
-Route::delete('products/{product}', 
+Route::delete('products/{product}',
     [ProductController::class, 'delete'])->name('products.destroy');
