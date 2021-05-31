b51fe5361dd751aa6e7310f65c39e0cf78dacc11
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-05-31 20:23:13 +0200

    using seeders for products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(1),
+            'price' => $this->faker->randomFloat($maxDecimals = 2, $min = 3, $max = 100),
+            'stock' => $this->faker->numberBetween(1, 10),
+            'status' => $this->faker->randomElement(['available', 'unavailable']),
+        ];
+    }
+}
