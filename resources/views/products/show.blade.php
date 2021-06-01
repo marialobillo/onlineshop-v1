8abe1e81376c263e0221b9910c44703a965e0110
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-01 19:23:41 +0200

    partials

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,17 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product - {{ $product->title  }}</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('content')
     <div class="container">
         <h1>{{ $product->title  }} ({{ $product->id  }})</h1>
         <p>{{ $product->description }}</p>
         <p>{{ $product->price  }}</p>
     </div>
-</body>
-</html>
+    @endsection
+
+
