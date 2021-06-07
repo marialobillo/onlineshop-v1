8bea3ac03a0348048012a8738d8aee9444176729
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-07 20:29:45 +0200

    showing errors

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,16 @@
         </div>
     @endif
 
+    @if(isset($errors) || $errors->any())
+        <div class="alert alert-danger">
+           <ul>
+               @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+               @endforeach
+           </ul>
+        </div>
+    @endif
+
     @yield('content')
 </body>
 </html>
