c8ec81585f365119521654cab7398de5980266cb
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-13 19:48:20 +0200

    udpating images, orders, payments migrations

--- database/migrations/2021_06_12_170434_create_orders_table.php
+++ database/migrations/2021_06_12_170434_create_orders_table.php
@@ -15,6 +15,8 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status')->default('pending');
+            // costumer_id
             $table->timestamps();
         });
     }
