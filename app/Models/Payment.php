e16e9ce3f315d53d59f1b8ad80c9aaa8f9ebe1fa
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-13 20:30:27 +0200

    protected fields

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'amount',
+        'payed_at',
+    ];
+
+    protected $dates = [
+        'payed_at'
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = [
+        'admin_since ',
+    ];
 }
