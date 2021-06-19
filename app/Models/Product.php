2206a0e51a9619baa4c4b23a7ef82957248cdcd4
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-19 20:07:11 +0200

    update product for attach images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -33,4 +33,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class)->withPivot('quantity');
     }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
