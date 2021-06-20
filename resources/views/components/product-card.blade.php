a31ad2c1eb1caffeccf12f031709ec67aea0f138
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-20 14:24:44 +0200

    updating the view for listing products

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,5 +1,11 @@
-<h2>{{ $product->title }} / {{ $product->id }}</h2>
-<p>{{ $product->description }}</p>
-<p>${{ $product->price }}</p>
-<p>Stock: {{ $product->stock }}</p>
-<p>{{ $product->status }}</p>
+
+<div class="card">
+    <img src="{{ asset($product->images->first()->path) }}" alt="" class="card-img" max-height="500">
+    <div class="card-body">
+        <h4 class="card-title"><strong>${{ $product->price }}</strong></h4>
+        <h5 class="card-title">{{ $product->title }}</h5>
+        <p class="card-text">{{ $product->description }}</p>
+        <p class="card-text"><strong>{{ $product->stock }} left</strong></p>
+    </div>
+</div>
+
