c6d3aeece84558488aa81874a77552f0426161e9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-06-20 13:43:35 +0200

    update the seed for users, orders, and payments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Payment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $products = Product::factory(20)->create();
+        $products = Product::factory(30)->create();
+
+        $users = User::factory(15)->create();
+
+        $orders = Order::factory(10)
+            ->make()
+            ->each(function($order) use ($users){
+                $order->customer_id = $users->random()->id;
+                $order->save();
+
+                $payment = Payment::factory()->make();
+//                $payment->order_id = $order->id;
+//                $payment->save();
+
+                $order->payment()->save($payment);
+            });
     }
 }
