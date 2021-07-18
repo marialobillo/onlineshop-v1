7d2a6654fcfb8ea6ba8a7aceb635ed1ed0927b11
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-07-18 19:14:06 +0200

    some tests

---
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get(route('home'));
+
+        // $response->assertStatus(200);
+        // $response->assertViewIs('home');
+    }
+}
