f85eb30f138b9e97f4aa08ab04e59a9ab75d5d9f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-07-18 20:02:29 +0200

    udpate user profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,9 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->admin_since != null
             && $this->admin_since->lessThanOrEqualTo(now());
     }
+
+    public function setPasswordAttribute($password){
+
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
