c83558ca0be6035c2e9ec97f7922d4e8bb7450cd
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-08-13 12:54:38 +0200

    cleaning controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', [MainController::class, 'index'])->name('main');
-
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-
-Route::resource('products.carts', 'ProductCartController')->only(['store', 'destroy']);
-
-Route::resource('carts', 'CartController')->only(['index']);
-
-Route::resource('orders', 'OrderController')
-    ->only(['create', 'store'])
-    ->middleware(['verified']);
-
-Route::resource('orders.payments', 'OrderPaymentController')
-    ->only(['create', 'store'])
-    ->middleware(['verified']);
-
-Auth::routes([
-    'verify' => true,
-//    'reset' => true
-]);
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
